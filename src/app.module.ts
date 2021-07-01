7dcae4c809053e2b06466ee0000f673e7c814fc9
koudai0024 <iwai.0731v@gmail.com>
2021-07-01 15:01:52 +0900

    ✨ routesのapiルート作成開始

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { TouristSpotsModule } from './tourist-spots/tourist-spots.module';
 import { StationsModule } from './stations/stations.module';
+import { RoutesModule } from './routes/routes.module';
 
 @Module({
-  imports: [PrismaModule, TouristSpotsModule, StationsModule],
+  imports: [PrismaModule, TouristSpotsModule, StationsModule, RoutesModule],
   controllers: [],
   providers: [],
 })
